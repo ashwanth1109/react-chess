dcc831dda674744199457481a8101dc8b54b640e
Ashwanth A R <ashwanth1109@gmail.com>
2019-09-08 23:59:11 +0530

    Queen's movements coded

--- src/rules/queen.js
+++ src/rules/queen.js
@@ -1,38 +1,79 @@
 const moveQueen = (clickedPiece, nextSquare, moveSuccessfully) => {
-  console.log("moving queen");
-  const currentSquare = clickedPiece.square;
-
   const rule = () => {
-    const moveUp = () => {
-      let checkSquare = currentSquare;
-      console.log(checkSquare);
-      if (checkSquare.up !== null) {
-        while (checkSquare.up !== null && checkSquare.up.piece === null) {
-          //   console.log("iterating loop", checkSquare);
-          if (checkSquare.up === nextSquare) return true;
-          checkSquare = checkSquare.up;
-        }
-        if (checkSquare.up.piece.player !== clickedPiece.player) return true;
-      }
+    const checkMove = direction => {
+      let currentSquare = clickedPiece.square;
 
+      // if square exists in that direction
+      if (currentSquare[direction] !== null) {
+        // queen can move any number of squares in a direction
+        // while squares still exist in direction and queen doesnt encounter a piece
+        while (
+          currentSquare[direction] !== null &&
+          currentSquare[direction].piece === null
+        ) {
+          // check if square is where user wants to move queen
+          if (currentSquare[direction] === nextSquare) return true;
+          // keep moving in the same direction
+          currentSquare = currentSquare[direction];
+        }
+        // cant move further because edge of board or piece in the way
+        if (
+          currentSquare[direction] !== null &&
+          currentSquare[direction].piece !== null &&
+          currentSquare[direction] === nextSquare &&
+          currentSquare[direction].piece.player !== clickedPiece.player
+        )
+          return true;
+      }
       return false;
     };
 
-    const moveDown = () => {
-      let checkSquare = currentSquare;
-      if (checkSquare.down !== null) {
-        while (checkSquare.down !== null && checkSquare.down.piece === null) {
-          if (checkSquare.down === nextSquare) return true;
-          checkSquare = checkSquare.down;
+    const checkDiagonal = (direction1, direction2) => {
+      let currentSquare = clickedPiece.square;
+      // if square exists in the combination of directions
+      if (
+        currentSquare[direction1] !== null &&
+        currentSquare[direction1][direction2] !== null
+      ) {
+        // queen can move any number of squares in a diagonal direction
+        // as long as squares still exist and there are no pieces on them
+        while (
+          currentSquare[direction1] !== null &&
+          currentSquare[direction1][direction2] !== null &&
+          currentSquare[direction1][direction2].piece === null
+        ) {
+          // check if square is where user wants to move queen
+          if (currentSquare[direction1][direction2] === nextSquare) return true;
+          // keep moving in the same direction
+          currentSquare = currentSquare[direction1][direction2];
         }
-        if (checkSquare.down.piece.player !== clickedPiece.player) return true;
+        // cant move further because edge of board or piece in the way
+        if (
+          currentSquare[direction1] !== null &&
+          currentSquare[direction1][direction2] !== null &&
+          currentSquare[direction1][direction2].piece !== null &&
+          currentSquare[direction1][direction2] === nextSquare &&
+          currentSquare[direction1][direction2].piece.player !==
+            clickedPiece.player
+        )
+          return true;
       }
       return false;
     };
 
-    return moveUp() || moveDown();
+    return (
+      checkMove("up") ||
+      checkMove("down") ||
+      checkMove("left") ||
+      checkMove("right") ||
+      checkDiagonal("up", "left") ||
+      checkDiagonal("up", "right") ||
+      checkDiagonal("down", "left") ||
+      checkDiagonal("down", "right")
+    );
   };
 
+  // if movement rules allow the movement, then move is successful
   if (rule()) moveSuccessfully();
 };
 
